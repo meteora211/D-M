c590c30227ca3f7f807242a4ade52f77b57fac6a
Meteora <czhaor@gmail.com>
2018-06-21 20:51:15 +0800

    add data file

---
+++ MachineLearningInAction/chpt02-knn/datingTest/datingknn.py
@@ -0,0 +1,13 @@
+from numpy import *
+
+def importdata(filepath):
+    """
+    Function to load txt data to matrix
+    """
+    with open(filepath) as fileid:
+        content = fileid.readlines()
+    listdata = [item.strip('\n').split('\t') for item in content]
+    arraydata = array(listdata)
+    returnMat = arraydata[:,:3]
+    labelVector = arraydata[:,3] 
+    return returnMat, labelVector
